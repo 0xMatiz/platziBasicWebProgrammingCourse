b9f677115f8c1c862a0ecc310463581e97de9d6d
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-24 00:09:34 -0300

    update

---
+++ 03-Mokepon/mokeponJs.js
@@ -0,0 +1,8 @@
+/*Unpractical way to not deleting things 😅
+Monab -> PLANT       <!--1)Quirk "Humble Roots": Plants indestrutible roots that pass on to the next allie (PROTECTS FROM 1 LETHAL ATTACK)-->
+Nestorf -> WATER     <!--2)Quirk "Tide Tamer": Learns from each hit received and adapts to the circumstances(+RES +SP RES WHEN ATTACKED)-->
+Justoz -> FIRE       <!--3)Quirk "Fire Will": Has an unstoppable fire bursting from inside that spreads(PASSIVE FIRE DAMAGE ON AREA EACH TURN)-->
+Walz -> FIRE & PLANT <!--4)Quirk "Flaming Ground":-->
+Gerb -> WATER & PLANT<!--5)Quirk "Healing Sprout":-->
+Santz -> FIRE & WATER<!--6)Quirk "Reliable Vapor":-->
+ */
