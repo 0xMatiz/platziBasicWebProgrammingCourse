bbf44a0cedddef302b1a0f84be0604f66477cd97
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-24 00:28:29 -0300

    UPDATE

--- 03-Mokepon/mokepon.js
+++ 03-Mokepon/mokepon.js
@@ -5,4 +5,11 @@ Justoz -> FIRE       <!--3)Quirk "Fire Will": Has an unstoppable fire bursting f
 Walz -> FIRE & PLANT <!--4)Quirk "Flaming Ground":-->
 Gerb -> WATER & PLANT<!--5)Quirk "Healing Sprout":-->
 Santz -> FIRE & WATER<!--6)Quirk "Reliable Vapor":-->
+
+<script src="/03-Mokepon/mokepon.js"></script>
  */
+
+//when you put an id from html you cant use - you have to use _
+alert('Hello player, Welcom to MOKEPON!');
+
+let playerPartnerButton = document.getElementById('partner-button');
