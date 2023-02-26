9f81d1e140879d88b95de758f5f0a552811c78a2
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-26 03:05:48 -0300

    update

--- 03-Mokepon/mokepon.js
+++ 03-Mokepon/mokepon.js
@@ -3,27 +3,40 @@ function startGame () { //This function starts the game
     let playerPartnerButton = document.getElementById("partner-button");
     
     playerPartnerButton.addEventListener('click', selectPlayerPartner); //Add a listener to a 'click' event in the  webpage
+    selectOpponentPartner();
 }
 
 function selectPlayerPartner () { //This function throws a message with the partner you choose
-    //alert(`YOU CHOSE YOUR PARTNER`);
     let inputMonab = document.getElementById("monab");
     let inputNestorf = document.getElementById("nestorf");
     let inputJustoz = document.getElementById("justoz");
-    
+    let playerPartnerSpan = document.getElementById("player-partner")
+
     if(inputMonab.checked) {
-        alert(`You chose Monab as your partner!`);
+        playerPartnerSpan.innerHTML = 'Monab';
     } else if(inputNestorf.checked) {
-        alert(`You chose Nestorf as your partner!`);
+        playerPartnerSpan.innerHTML = 'Nestorf';
     } else if(inputJustoz.checked) {
-        alert(`You chose Justoz as your partner!`);
+        playerPartnerSpan.innerHTML = 'Justoz';
     } else {
         alert(`You didn't choose any partner`)
     }
 }
+//CODE TESTING AREA
+function selectOpponentPartner () { //I am still thinking how to do this part of the code.
+    selectPlayerPartner();
+    let opponentPartnerSpan = document.getElementById("opponent-partner")
 
-//VARIABLE DECLARING
-//const partnerNameList = [partnerMonab, partnerNestorf, partnerJustoz];
+    if(inputMonab.checked) {
+        opponentPartnerSpan.innerHTML = 'Justoz';
+    } else if(inputNestorf.checked) {
+        opponentPartnerSpan.innerHTML = 'Monab';
+    } else if(inputJustoz.checked) {
+        opponentPartnerSpan.innerHTML = 'Nestorf';
+    } else {
+        
+    }
+}
 
 //REGULAR CODE
 window.addEventListener('load', startGame);
