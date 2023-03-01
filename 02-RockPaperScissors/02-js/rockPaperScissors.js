133fb0df1a0788bd4349d800f5ec3a721701215e
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-03-01 00:50:29 -0300

    Update rockPaperScissors.js

--- 02-RockPaperScissors/02-js/rockPaperScissors.js
+++ 02-RockPaperScissors/02-js/rockPaperScissors.js
@@ -1,7 +1,10 @@
+//THIS CODE IS CURRENTLY ON REWORK TO MAKE IT BETTER.
+
+//Variable declaring start
+let player; 
+let pc;
+
 //Function Declaring Area Start
-function randomness (min, max) { //Declares a function that prints a round number between 1 and 3
-    return Math.round(Math.random() * (max - min + 1) + min);
-} 
 function election (move) { //Declared a function to print player and pc election
     let result = "";
     if (move = 1) {
@@ -15,33 +18,27 @@ function election (move) { //Declared a function to print player and pc election
     }
     return result;
 }
-//Function Declaring area end
 
-
-//Result
-if (pc == player ) {
-    alert("It's a tie");
-} else if (pc == 1 && player == 2) {
-    alert("You loose!");
-} else if (pc == 1 && player == 3) {
-    alert("You win!");
-} else {
-    alert('You choose to loose!');
+function combat () {
+    if (pc == player ) {
+        alert("It's a tie");
+    } else if (pc == 1 && player == 2) {
+        alert("You loose!");
+    } else if (pc == 1 && player == 3) {
+        alert("You win!");
+    } else {
+        alert('You choose to loose!');
+    }
 }
 
-
-//Variable declaring start
-let player; 
-let pc;
-let min = 1
-let max = 2
-let wins = 0;
-let losses = 0;
-//Variable declaring end
-
-
+function randomness (min, max) { //Declares a function that prints a round number between 1 and 3
+    return Math.round(Math.random() * (max - min + 1) + min);
+} 
 //Normal Code Area Start
 while (wins < 3 & losses < 3) {
+    let wins = 0;
+    let losses = 0;
+    
     player = prompt("Choose: 1 for rock, 2 for paper and 3 for scissors");// 1 is Rock, 2 is paper, 3 is scissors
     pc = randomness(1, 2); //makes PC value equal randomness function result
     alert("You choose: " + election(player) + "!");//Player election function call
